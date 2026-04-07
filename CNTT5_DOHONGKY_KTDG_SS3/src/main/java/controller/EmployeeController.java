9002e4ec3904d4947b8c7d999bed2657d824f6f2
hongky06 <dohongky2006@gmail.com>
2026-04-07 07:26:49 +0700

    Add files via upload

---
+++ CNTT5_DOHONGKY_KTDG_SS3/src/main/java/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import com.rk.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+
+    @GetMapping("/employees")
+    public String getEmployees(Model model) {
+
+        List<Employee> list = new ArrayList<>();
+
+        list.add(new Employee(1, "Nguyen Van A", "Dao tao", 12000));
+        list.add(new Employee(2, "Tran Thi B", "Dao tao", 8000));
+        list.add(new Employee(3, "Le Van C", "Dao tao", 15000));
+
+        model.addAttribute("employees", list);
+
+        return "employee-list";
+    }
+}
